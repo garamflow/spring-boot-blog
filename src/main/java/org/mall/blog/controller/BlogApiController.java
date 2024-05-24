178cac28d9d62e974c74a698f7cf436cb3c24189
garamflow <garamflow@gmail.com>
2024-05-24 23:55:04 +0900

    feat: 블로그 글 전체 조회를 위한 DTO와 service, controller 구현

--- src/main/java/org/mall/blog/controller/BlogApiController.java
+++ src/main/java/org/mall/blog/controller/BlogApiController.java
@@ -3,12 +3,16 @@ package org.mall.blog.controller;
 import lombok.RequiredArgsConstructor;
 import org.mall.blog.domain.Article;
 import org.mall.blog.dto.AddArticleRequest;
+import org.mall.blog.dto.ArticleResponse;
 import org.mall.blog.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,5 +26,16 @@ public class BlogApiController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
