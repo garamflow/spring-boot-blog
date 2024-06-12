0c8ec03efe382c655977b80cc53f11ac73382e57
garamflow <garamflow@gmail.com>
2024-06-12 23:53:52 +0900

    feat: 블로그 글 하나 조회 service, controller, 단위테스트 구현

--- src/test/java/org/mall/blog/controller/BlogApiControllerTest.java
+++ src/test/java/org/mall/blog/controller/BlogApiControllerTest.java
@@ -97,4 +97,27 @@ class BlogApiControllerTest {
                 .andExpect(jsonPath("$[0].content").value(content))
                 .andExpect(jsonPath("$[0].title").value(title));
     }
+
+    @DisplayName("findById: 블로그 글 조회에 성공한다.")
+    @Test
+    public void findById() throws Exception {
+        // given
+        final String url = "/api/articles";
+        final String title = "title";
+        final String content = "content";
+
+        Article savedArticle = blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(get(url, savedArticle.getId()));
+
+        // then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value(content))
+                .andExpect(jsonPath("$.title").value(title));
+    }
 }
